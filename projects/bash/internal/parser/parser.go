7cb36c16ec3a0fb9c04a217248d42989016bea66
Maxim Bordyugov <maxmutant1@yandex.ru>
2023-10-03 18:21:48 +0300

    add comments

--- projects/bash/internal/parser/parser.go
+++ projects/bash/internal/parser/parser.go
@@ -5,10 +5,11 @@ import (
 )
 
 type Command struct {
-	Command   string
-	Arguments []string
+	Command   string   // Command - character sequence, serves as identifier of which function to call
+	Arguments []string // Arguments - program arguments, doesn't have Command as first element
 }
 
+// ParseCommandLine - accepts string and splits it into sequence of commands separated with '|'.
 func ParseCommandLine(s string) ([]Command, error) {
 	var res []Command
 	chunks := strings.Split(s, "|") // todo - cmd || cmd будет страдать, се ля ви, потом переделаем
